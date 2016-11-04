f173f27e1f4638ca9a2592408ec2cf0be66806e5
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-11-04 11:08:33 +0100

    move files

--- src/language.ts
+++ src/language.ts
@@ -2,5 +2,5 @@
  * Created by Samuel Gratzl on Fri, 04 Nov 2016 09:47:30 GMT.
  */
 
-export const APP_NAME = 'LineUp Demos';
+export const APP_NAME = 'lineup_demos';
 export const HELLO_WORLD = 'Hello Phovea World';
