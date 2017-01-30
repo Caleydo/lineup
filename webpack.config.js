1aac1313cee0998e9b0e268b7b7efba07f7e79be
Samuel Gratzl <samuel.gratzl@jku.at>
2017-01-30 12:18:44 +0100

    define NODE_ENV for production and development mode

--- webpack.config.js
+++ webpack.config.js
@@ -139,6 +139,7 @@ function generateWebpack(options) {
       }),
       //define magic constants that are replaced
       new webpack.DefinePlugin({
+        NODE_ENV: JSON.stringify(options.isProduction ? 'production': 'development'),
         __VERSION__: JSON.stringify(pkg.version),
         __LICENSE__: JSON.stringify(pkg.license),
         __BUILD_ID__: buildId,
